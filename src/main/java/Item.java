e197f2aec5995bebef83b23407bd5d9445899949
ecawthorne <55034881+ecawthorne@users.noreply.github.com>
2019-10-22 22:04:48 -0400

    Added isHidden variable and overloaded constructor

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -11,11 +11,18 @@ public class Item
     private String name = null;
     //Description of item
     private String desc = null;
+    boolean isHidden = false;
 
     public Item(String name, String desc)
     {
         this.name = name;
         this.desc = desc;
+    }
+    public Item(String name, String desc, boolean isHidden)
+    {
+        this.name = name;
+        this.desc = desc;
+        this.isHidden = true;
     }
     
     public void setName(String name)
