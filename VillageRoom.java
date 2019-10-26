d0f807a390e0130371409aa3d432de5f664b4528
Eric <eccawthorne@gmail.com>
2019-10-26 13:59:33 -0400

    Commented out constructors for testing purposes

--- VillageRoom.java
+++ VillageRoom.java
@@ -1,3 +1,4 @@
+//Dummy class for the moment
 
 /**
  *
@@ -8,31 +9,31 @@ public class VillageRoom extends Room
 
     VillageRoom()
     {
+        setName("The Village");
     }
 
     public VillageRoom(String n, int loc)
     {
-        super(n, loc);
+        //super(n, loc);
     }
 
-    public VillageRoom(String n, int descLoc, Room connectedRoom)
-    {
-        super(n, descLoc, connectedRoom);
-    }
-
-    public VillageRoom(String n, int loc, Room conRoom1, Room conRoom2)
-    {
-        super(n, loc, conRoom1, conRoom2);
-    }
-
-    public VillageRoom(String n, int loc, Room conRoom1, Room conRoom2, Room conRoom3)
-    {
-        super(n, loc, conRoom1, conRoom2, conRoom3);
-    }
-
-    public VillageRoom(String n, int loc, Room conRoom1, Room conRoom2, Room conRoom3, Room conRoom4)
-    {
-        super(n, loc, conRoom1, conRoom2, conRoom3, conRoom4);
-    }
-
+//    public VillageRoom(String n, int descLoc, Room connectedRoom)
+//    {
+//        super(n, descLoc, connectedRoom);
+//    }
+//
+//    public VillageRoom(String n, int loc, Room conRoom1, Room conRoom2)
+//    {
+//        super(n, loc, conRoom1, conRoom2);
+//    }
+//
+//    public VillageRoom(String n, int loc, Room conRoom1, Room conRoom2, Room conRoom3)
+//    {
+//        super(n, loc, conRoom1, conRoom2, conRoom3);
+//    }
+//
+//    public VillageRoom(String n, int loc, Room conRoom1, Room conRoom2, Room conRoom3, Room conRoom4)
+//    {
+//        super(n, loc, conRoom1, conRoom2, conRoom3, conRoom4);
+//    }
 }
