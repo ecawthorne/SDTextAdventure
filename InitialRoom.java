b969c51d4c6e871adab70818b629d238690395c3
Eric <eccawthorne@gmail.com>
2019-11-09 22:34:41 -0500

    Refactored consturctors, moved itemList from superclass into InitialRoom itself

--- InitialRoom.java
+++ InitialRoom.java
@@ -7,58 +7,30 @@ import java.util.Scanner;
  */
 class InitialRoom extends Room
 {
-
-    //This is here for testing. Add functionality where all descriptions are
-    //stored in and then read from a file 
-    final String intro = "\"Once upon a time, there was a lonely orphan farmer\n"
+    private ArrayList<Item> itemList = new ArrayList<>();
+    //Sets up room title and descriptions and adds items that will be in the room to
+    //the rooms inventory
+    InitialRoom()
+    {
+        super("Your house", "\"Once upon a time, there was a lonely orphan farmer\n"
             + "boy who was having a sentimental dream about his pet dog. \n"
             + "Suddenly, he started sweating from the rising temperatures\n"
             + "in his room. The farmer boy wakes up to see his room up in\n"
             + "flames and hears war cries outside! He calls out for his dog,\n"
             + "\"Bubbles, where are you!?\" The dog doesn\'t respond and is \n"
             + "nowhere to be seen. He sits up and hears ogres making crying\n"
-            + "out for war. \"Nobody messes with my dog!\"";
-    //Sets up room descriptions and adds item that will be in the room to
-    //the rooms inventory
-    InitialRoom()
-    {
-        setName("Your house");
-        setIntro(this.intro);
-        setInternalDesc("There are haystacks up in flames on his right. "
-                + "To his left is his wardrobe, and in front of him is a"
-                + " mirror. Where should he move?");
+            + "out for war. \"Nobody messes with my dog!\"",
+              "There are haystacks up in flames on his right. "
+            + "To his left is his wardrobe, and in front of him is a"
+            + " mirror. Where should he move?");
         ItemContainer wardrobe = new ItemContainer("Wardrobe", "It's a wardrobe.");
         wardrobe.addItem(new Item("Pitchfork", "The pitchfork can be used in "
                 + "battles or when cultivating. Be wise with it!"));
-        addItem(wardrobe);
-        addItem(new Item("Mirror", "You look handsome...but burnt."));
-        setLeavable(false);
+        this.itemList.add(wardrobe);
+        this.itemList.add(new Item("Mirror", "You look handsome...but burnt."));
+        super.setLeavable(true);
     }
-
-//    public InitialRoom(String n, String intro)
-//    {
-//        super(n, intro);
-//    }
-//
-//    public InitialRoom(String n, int descLoc, Room connectedRoom)
-//    {
-//        super(n, descLoc, connectedRoom);
-//    }
-//
-//    public InitialRoom(String n, int loc, Room conRoom1, Room conRoom2)
-//    {
-//        super(n, loc, conRoom1, conRoom2);
-//    }
-//
-//    public InitialRoom(String n, int loc, Room conRoom1, Room conRoom2, Room conRoom3)
-//    {
-//        super(n, loc, conRoom1, conRoom2, conRoom3);
-//    }
-//
-//    public InitialRoom(String n, int loc, Room conRoom1, Room conRoom2, Room conRoom3, Room conRoom4)
-//    {
-//        super(n, loc, conRoom1, conRoom2, conRoom3, conRoom4);
-//    }
+    
     @Override
     public String getLeaveCondition()
     {
