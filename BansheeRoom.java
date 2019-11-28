c310f5fc9e5f99d2921d238a017ab1778ba6293e
Eric <eccawthorne@gmail.com>
2019-11-28 16:16:20 -0500

    Added dummy class, will be updated soon

---
+++ BansheeRoom.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eric
+ */
+public class BansheeRoom extends Room
+{
+    BansheeRoom(String name, String desc)
+    {
+        super(name, desc);
+    }
+}
